7324c46de28ac16f70659dc4299d53a4a80d30f5
toryherman <tory.herman12@gmail.com>
2016-12-12 23:29:13 -0600

    push messages to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,25 +9,55 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class AppComponent {
   title = 'Bloc Chat';
   rooms: FirebaseListObservable<any>;
+  messages: FirebaseListObservable<any>;
   modal: boolean;
   roomName: string;
+  currentRoomId: string;
+  currentMessage: string;
+  userName: string;
   
   constructor(public af: AngularFire) {
     this.rooms = af.database.list('/rooms');
+    this.messages = af.database.list('/messages');
     this.modal = false;
     this.roomName = "";
+    this.currentRoomId = "";
+    this.currentMessage = "";
+    this.userName = "";
   }
   
   login() {
     this.af.auth.login();
+    //this.userName = af.auth.subscribe( );
   }
 
   logout() {
     this.af.auth.logout();
+    this.userName = "";
   }
 
   addRoom() {
     this.rooms.push(this.roomName);
     this.modal = false;
+  }
+
+  setCurrentRoom(key) {
+    this.currentRoomId = key;
+    console.log(key);
+  }
+
+  sendMessage() {
+    if (this.currentRoomId !== "") {
+      let message = {
+        username: "#",
+        content: this.currentMessage,
+        sentAt: "0:00",
+        roomId: this.currentRoomId
+      };
+      
+      this.messages.push(message);
+
+      this.currentMessage = "";
+    }
   }
 }
